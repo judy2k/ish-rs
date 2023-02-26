84933c37a9e6ab9410d14d3400f51ec6e53c0d69
Judy2k <judy@judy.co.uk>
2023-02-26 12:03:54 +0000

    Strings and other integral types.

--- src/lib.rs
+++ src/lib.rs
@@ -33,10 +33,6 @@ impl BoolIsh {
     fn is_true_string(&self, s: &str) -> bool {
         s.to_lowercase().trim().eq("true")
     }
-
-    fn is_true_u64(&self, s: u64) -> bool {
-        s == 1
-    }
 }
 
 impl cmp::PartialEq<&str> for BoolIsh {
@@ -48,14 +44,72 @@ impl cmp::PartialEq<&str> for BoolIsh {
         }
     }
 }
+impl cmp::PartialEq<String> for BoolIsh {
+    fn eq(&self, other: &String) -> bool {
+        if self.value {
+            self.is_true_string(other.as_str())
+        } else {
+            !self.is_true_string(other.as_str())
+        }
+    }
+}
+
+fn i64_fuzzy_eq(value: bool, other: i64) -> bool {
+    if value {
+        other == 1
+    } else {
+        other != 1
+    }
+}
 
 impl cmp::PartialEq<u8> for BoolIsh {
     fn eq(&self, other: &u8) -> bool {
-        if self.value {
-            self.is_true_u64(*other as u64)
-        } else {
-            !self.is_true_u64(*other as u64)
-        }
+        i64_fuzzy_eq(self.value, *other as i64)
+    }
+}
+impl cmp::PartialEq<u16> for BoolIsh {
+    fn eq(&self, other: &u16) -> bool {
+        i64_fuzzy_eq(self.value, *other as i64)
+    }
+}
+impl cmp::PartialEq<u32> for BoolIsh {
+    fn eq(&self, other: &u32) -> bool {
+        i64_fuzzy_eq(self.value, *other as i64)
+    }
+}
+impl cmp::PartialEq<u64> for BoolIsh {
+    fn eq(&self, other: &u64) -> bool {
+        i64_fuzzy_eq(self.value, *other as i64)
+    }
+}
+impl cmp::PartialEq<usize> for BoolIsh {
+    fn eq(&self, other: &usize) -> bool {
+        i64_fuzzy_eq(self.value, *other as i64)
+    }
+}
+impl cmp::PartialEq<i8> for BoolIsh {
+    fn eq(&self, other: &i8) -> bool {
+        i64_fuzzy_eq(self.value, *other as i64)
+    }
+}
+impl cmp::PartialEq<i16> for BoolIsh {
+    fn eq(&self, other: &i16) -> bool {
+        i64_fuzzy_eq(self.value, *other as i64)
+    }
+}
+impl cmp::PartialEq<i32> for BoolIsh {
+    fn eq(&self, other: &i32) -> bool {
+        i64_fuzzy_eq(self.value, *other as i64)
+    }
+}
+impl cmp::PartialEq<i64> for BoolIsh {
+    fn eq(&self, other: &i64) -> bool {
+        i64_fuzzy_eq(self.value, *other as i64)
+    }
+}
+impl cmp::PartialEq<isize> for BoolIsh {
+    fn eq(&self, other: &isize) -> bool {
+        i64_fuzzy_eq(self.value, *other as i64)
     }
 }
 
@@ -81,11 +135,13 @@ mod tests {
         assert_eq!(trueish, "true");
         assert_eq!(trueish, "True");
         assert_eq!(trueish, "TRUE");
+        assert_eq!(trueish, "TRUE".to_owned());
         assert_eq!(trueish, 1);
 
         assert!(trueish != "false");
         assert!(trueish != "False");
         assert!(trueish != "FALSE");
+        assert!(trueish != "FALSE".to_owned());
         assert!(trueish != 0);
     }
 
@@ -96,11 +152,13 @@ mod tests {
         assert_eq!(falseish, "false");
         assert_eq!(falseish, "False");
         assert_eq!(falseish, "FALSE");
+        assert_eq!(falseish, "FALSE".to_owned());
         assert_eq!(falseish, 0);
 
         assert!(falseish != "true");
         assert!(falseish != "True");
         assert!(falseish != "TRUE");
+        assert!(falseish != "TRUE".to_owned());
         assert!(falseish != 1);
     }
 }
